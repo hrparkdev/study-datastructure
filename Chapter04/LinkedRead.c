1e04c484fcb41f262ac965702a9bcda04556aba9
hrparkdev <hrparkdev@gmail.com>
2024-06-23 18:24:17 +0900

    Chapter04

---
+++ Chapter04/LinkedRead.c
@@ -0,0 +1,85 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct _node
+{
+	int data; //데이터를 담을 공간
+	struct _node* next; //연결의 도구! (Node형 구조체 변수의 주소값을 저장할 수 있는 포인터 변수)
+} Node;
+
+int main(void)
+{
+	Node* head = NULL; //리스트의 머리를 가리키는 포인터 변수
+	Node* tail = NULL; //리스트의 꼬리를 가리키는 포인터 변수
+	Node* cur = NULL; //저장된 데이터의 조회에 사용되는 포인터 변수
+
+	Node* newNode = NULL;
+	int readData;
+
+	// 데이터를 입력 받는 과정 ///////
+	while (1) 
+	{
+		printf("자연수 입력: ");
+		scanf("%d", &readData);
+		if (readData < 1)
+			break;
+
+		//노드의 추가과정
+		newNode = (Node*)malloc(sizeof(Node)); //노드(바구니)의 생성
+		newNode->data = readData; //노드에 데이터 저장
+		newNode->next = NULL; //노드의 next를 NULL로 초기화
+
+		if (head == NULL) //첫 번째 노드라면!
+			head = newNode; //첫 번째 노드를 head가 가리키게 함
+		else //두 번째 이후 노드라면!
+			tail->next = newNode;     
+
+		tail = newNode; //노드의 끝을 tail이 가리키게 함
+	}
+	printf("\n");
+
+	// 입력 받은 데이터의 출력과정 ///////
+	printf("입력 받은 데이터의 전체출력! \n");
+	if (head == NULL)
+	{
+		printf("저장된 자연수가 존재하지 않습니다. \n");
+	}
+	else
+	{
+		cur = head; //cur이 리스트의 첫 번째 노드를 가리킨다.
+		printf("%d ", cur->data); //첫 번째 데이터 출력
+
+		while (cur->next != NULL) //연결된 노드가 존재한다면
+		{
+			cur = cur->next; //cur가 다음 노드를 가리키게 한다.
+			printf("%d ", cur->data); //cur이 가리키는 노드를 출력한다.
+		}
+	}
+	printf("\n\n");
+
+	// 메모리의 해제과정 ///////
+	if (head == NULL)
+	{
+		return 0; //삭제할 노드가 존재하지 않는다.
+	}
+	else
+	{
+		Node* delNode = head;
+		Node* delNextNode = head->next;
+
+		printf("%d을(를) 삭제합니다. \n", head->data);
+		free(delNode); //첫 번째 노드 삭제
+
+		while (delNextNode != NULL) //두 번째 이후 노드 삭제
+		{
+			delNode = delNextNode;
+			delNextNode = delNextNode->next;
+
+			printf("%d을(를) 삭제합니다. \n", delNode->data);
+			free(delNode);
+		}
+	}
+
+	return 0;
+}
---
+++ Chapter04/PointListSortMain.c
@@ -0,0 +1,92 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "DLinkedList.h"
+#include "Point.h"
+
+int WhoIsPrecede(Point* d1, Point* d2)
+{
+	if (d1->xpos < d2->xpos)
+	{
+		return 0;
+	}
+	else if (d1->xpos == d2->xpos)
+	{
+		if (d1->ypos < d2->ypos)
+			return 0;
+		else
+			return 1;
+	}
+	else
+		return 1;
+}
+
+int main(void)
+{
+	List list;
+	Point comp;
+	Point* pPoint;
+	ListInit(&list);
+
+	SetSortRule(&list, WhoIsPrecede);   
+
+	pPoint = (Point*)malloc(sizeof(Point));
+	SetPointPos(pPoint, 3, 2);
+	LInsert(&list, pPoint);
+
+	pPoint = (Point*)malloc(sizeof(Point));
+	SetPointPos(pPoint, 3, 1);
+	LInsert(&list, pPoint);
+
+	pPoint = (Point*)malloc(sizeof(Point));
+	SetPointPos(pPoint, 2, 2);
+	LInsert(&list, pPoint);
+
+	pPoint = (Point*)malloc(sizeof(Point));
+	SetPointPos(pPoint, 2, 1);
+	LInsert(&list, pPoint);
+
+	printf("현재 데이터의 수: %d \n", LCount(&list));
+
+	if (LFirst(&list, &pPoint))
+	{
+		ShowPointPos(pPoint);
+
+		while (LNext(&list, &pPoint))
+			ShowPointPos(pPoint);
+	}
+	printf("\n");
+
+	comp.xpos = 2;
+	comp.ypos = 0;
+
+	if (LFirst(&list, &pPoint))
+	{
+		if (PointComp(pPoint, &comp) == 1)
+		{
+			pPoint = LRemove(&list);
+			free(pPoint);
+		}
+
+		while (LNext(&list, &pPoint))
+		{
+			if (PointComp(pPoint, &comp) == 1)
+			{
+				pPoint = LRemove(&list);
+				free(pPoint);
+			}
+		}
+	}
+
+	printf("현재 데이터의 수: %d \n", LCount(&list));
+
+	if (LFirst(&list, &pPoint))
+	{
+		ShowPointPos(pPoint);
+
+		while (LNext(&list, &pPoint))
+			ShowPointPos(pPoint);
+	}
+	printf("\n");
+
+	return 0;
+}
